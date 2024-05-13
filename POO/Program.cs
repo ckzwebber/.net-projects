015ee2d531eb2f470166c29b55502238ca3b3e3e
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-05-13 10:33:45 -0300

    Fix

--- POO/Program.cs
+++ POO/Program.cs
@@ -4,37 +4,37 @@ using POO.Models;
 ICalculadora calculadora = new Calculadora();
 Console.WriteLine(calculadora.Somar(10, 20));
 
-// Computador c = new Computador();
+Computador c = new Computador();
 
 
-// Aluno a1 = new Aluno("Carlos");
+Aluno a1 = new Aluno("Carlos");
 
-// Corrente c = new Corrente();
-// c.Creditar(500);
-// c.ExibirSaldo();
+Corrente c = new Corrente();
+c.Creditar(500);
+c.ExibirSaldo();
 
 
-// Aluno a1 = new Aluno();
-// a1.Nome = "Carlos";
-// a1.Idade = 30;
-// a1.Email = "test@gmail.com";
-// a1.Nota = 10;
-// a1.Apresentar();
+Aluno a1 = new Aluno();
+a1.Nome = "Carlos";
+a1.Idade = 30;
+a1.Email = "test@gmail.com";
+a1.Nota = 10;
+a1.Apresentar();
 
-// Professor p1 = new Professor();
-// p1.Nome = "Miguel";
-// p1.Idade = 30;
-// p1.Salario = 3000;
-// p1.Apresentar();
+Professor p1 = new Professor();
+p1.Nome = "Miguel";
+p1.Idade = 30;
+p1.Salario = 3000;
+p1.Apresentar();
 
-// ContaCorrente c1 = new ContaCorrente(123, 1000);
+ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-// c1.ExibirSaldo();
-// c1.Sacar(3000);
-// c1.ExibirSaldo();
+c1.ExibirSaldo();
+c1.Sacar(3000);
+c1.ExibirSaldo();
 
-// Pessoa p1 = new Pessoa();
-// p1.Nome = "Carlos";
-// p1.Idade = 30;
+Pessoa p1 = new Pessoa();
+p1.Nome = "Carlos";
+p1.Idade = 30;
 
-// p1.Apresentar();
+p1.Apresentar();
